85bfa6852837ade9bccab357ebcdac7f97aff6e6
Carlos Martin Garcia Madrigal <75263278+Carlos-Garcia09@users.noreply.github.com>
2024-01-10 04:13:53 -0600

    Update urls.py

--- PlantApp/urls.py
+++ PlantApp/urls.py
@@ -1,27 +1,28 @@
-"""
-URL configuration for PlantApp project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from app import views
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('about/', views.about, name='about')
+    path('analisis/', views.analisis, name='analisis'),
+    path('login/', views.user_login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('logout/', views.user_logout, name='logout'),
+    path('add_plant/', views.add_plant, name='add_plant'),
+    path('mi_jardin/', views.mi_jardin, name='mi_jardin'),
+    path('eliminar_bitacora/<int:id_bitacora>/', views.eliminar_bitacora, name='eliminar_bitacora'),
+
+    path('bitacora_planta/<int:planta_id>/', views.bitacora_planta, name='bitacora_planta'),
+    path('mi_jardin/detalle_planta/<int:id_planta>/', views.detalle_planta, name='detalle_planta'),
+    path('eliminar_planta/<int:id_planta>/', views.eliminar_planta, name='eliminar_planta'),
+    path('login-with-google/', views.login_with_google, name='login_with_google'),
+    path('complete/google-oauth2/', views.complete_google_auth, name='complete_google_auth'),
+    path('social-auth/', include('social_django.urls', namespace='social')),
 ]
 
-
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
